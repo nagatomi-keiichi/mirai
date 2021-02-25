fd049b9d1aef0f39ea989fc2f9381d07b8f9b762
nagatomikeiichi <nagatomi.keiichi@gmail.com>
2021-02-25 13:05:42 +0900

    背景を画像化

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'diaries/index'
+  devise_for :users
+  root to: "diaries#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
