fc17a53349d2fe0a4abc3c931b4ca6e5e11b768a
nagatomikeiichi <nagatomi.keiichi@gmail.com>
2021-03-03 22:08:16 +0900

    カレンダー機能

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,4 +1,44 @@
 class DiariesController < ApplicationController
   def index
+    @diaries  = Diary.all
   end
+
+  def new
+    @diary = Diary.new
+  end
+
+  def show
+    @diary = Diary.find(params[:id])
+  end
+
+  def create
+    Diary.create(diary_parameter)
+    redirect_to diaries_path
+  end
+
+  def destroy
+    @diary = Diary.find(params[:id])
+    @diary.destroy
+    redirect_to diaries_path, notice:"削除しました"
+  end
+
+  def edit
+    @diary = Diary.find(params[:id])
+  end
+
+  def update
+    @diary = Diary.find(params[:id])
+    if @diary.update(diary_parameter)
+      redirect_to diaries_path, notice: "編集しました"
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def diary_parameter
+    params.require(:diary).permit(:title, :content, :start_time)
+  end
+
 end
